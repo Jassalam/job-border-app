5d3636616c59cf5dccd9cce51e1663a955083db1
JASVEER KAUR <jasveerk1980@gmail.com>
2023-04-24 17:00:45 +0200

    created setup file for new user entry

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import prisma from "@/lib/prisma"
 import { getJobs } from 'lib/data.js'
 import Jobs from "./components/Jobs"
-
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 
 
 
 export default function Index({jobs}) {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  if( session && !session.user.name){
+    router.push('/setup')
+  }
+
+
   return (
     <main
       className='first-letter:flex min-h-screen flex-col items-center justify-between p-24'
@@ -15,6 +24,13 @@ export default function Index({jobs}) {
       <div className='text-center p-4 m-4'>
         <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
       </div>
+      {!session && (
+        <a
+        className="flex justify-center w-24 px-8 py-2 mx-auto font-bold text-white bg-black border border-black rounded-full"
+        href='/api/auth/signin'>
+          Login
+        </a>
+      )}
      <Jobs jobs={jobs}/>
     </div>
   
