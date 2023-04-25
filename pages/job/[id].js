46462d637174594f34f480422fa69d1be115517a
JASVEER KAUR <jasveerk1980@gmail.com>
2023-04-25 15:42:24 +0200

    applied job status

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -1,10 +1,11 @@
 import { getJob, alreadyApplied } from "@/lib/data"
 import prisma from "@/lib/prisma"
-
+import { authOptions } from "../api/auth/[...nextauth]"
+import { getServerSession } from "next-auth"
 import Link from "next/link"
 
 
-export default function Job({job}){
+export default function Job({job, applied}){
 
     return(
         <div className="flex flex-col w-1/2 mx-auto">
@@ -24,13 +25,24 @@ export default function Job({job}){
                     <p className="text-base font-normal mt-3">
                         {job.description}
                     </p>
-                    <div className="mt-20 flex justify-center">
+                    {applied ?(
+                        <div className="mt-20 flex justify-center">
+                            <Link href={`/dashboard`}>
+                                <button className="border px-8 py-2 mt-0 font-bold rounded-full bg-black text-white">
+                                    You already applied!
+                                </button>
+                            </Link>
+                        </div>
+                    ):(
+                        <div className="mt-20 flex justify-center">
                         <Link href={`/job/${job.id}/apply`}>
                             <button className="border px-8 py-2 mt-0 font-bold rounded-full bg-black text-white">
                                 Apply to this job
                             </button>
                         </Link>
                     </div>
+                    )}
+                    
                     <div className="mt-4">
                     <h4 className="inline">
                         Posted by
@@ -55,23 +67,23 @@ export default function Job({job}){
 }
 
 export async function getServerSideProps(context){
-//const session = await getServerSession(context.req, context.res, authOptions)
+const session = await getServerSession(context.req, context.res, authOptions)
 
 
 
     let job = await getJob(context.params.id, prisma)
     job = JSON.parse(JSON.stringify(job))
 
-   /* const applied = await alreadyApplied(
+   const applied = await alreadyApplied(
        session.user.id,
         context.params.id,
         prisma
-    )*/
+    )
 
     return{ 
         props: {
             job,
-            //applied,
+            applied,
         },
     }
 }
